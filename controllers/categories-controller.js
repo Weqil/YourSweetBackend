c13c5cea8896b7809759baf6daff92f39d1a45bf
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-26 15:09:08 +0500

    fix cors error, add headers response, review router, add query args

--- controllers/categories-controller.js
+++ controllers/categories-controller.js
@@ -1,40 +1,37 @@
-const Categories = require('../models/categories')
+const Categories = require("../models/categories");
 
-
-module.exports.categoriesAll = function(req, res){
-    Categories.findAll().then((category)=>{
-        const json = JSON.stringify(category)
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(json)
-    }).catch((err)=>{
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Ошибка при получении списка фильмов');
+module.exports.categoriesAll = function (req, res, query) {
+  Categories.findAll()
+    .then((category) => {
+      const json = JSON.stringify(category);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(json);
     })
-}
+    .catch((err) => {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Ошибка при получении списка фильмов");
+    });
+};
 
-module.exports.createCategories = function(req,res){
-    let body = '';
-    req.on('data',(chunk)=>{
-        body = body + chunk.toString();
-    })
+module.exports.createCategories = function (req, res, body) {
+  req.on("end", () => {
+    try {
+      const data = JSON.parse(body);
+      Categories.create({
+        name: data.name,
+      })
+        .then(() => {
+          res.end("Данные внесенны успешно!");
+        })
+        .catch((err) => {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Ошибка при добавлении категории");
+        });
+    } catch (error) {
+      console.error("Ошибка при парсинге JSON: ", error);
 
-    req.on('end', ()=>{
-        try {
-            const data = JSON.parse(body);
-            Categories.create({
-                name: data.name
-            }).then(()=>{
-                res.end("Данные внесенны успешно!");
-            }).catch((err) => {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Ошибка при добавлении категории');
-            });
-        } catch(error) {
-            console.error("Ошибка при парсинге JSON: ", error);
-        
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end("Ошибка при обработке данных");
-        }
-    })
-
-}
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end("Ошибка при обработке данных");
+    }
+  });
+};
--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -4,6 +4,16 @@ const Categories = require("../models/categories");
 const { createAdmins } = require("./admins-controller");
 
 module.exports = function requestController(req, res) {
+  // Получаем query параметры
+  const url_parts = url.parse(req.url, true);
+  const query = url_parts.query;
+  // Получаем тело запроса
+  var body;
+  req.on("data", (chunk) => {
+    body = chunk.toString();
+  });
+
+  // Устанавливаем заголовки для CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -15,26 +25,59 @@ module.exports = function requestController(req, res) {
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  if (req.method === "GET" && req.url === "/admins") {
-    const { adminsAll } = require("./admins-controller");
-    adminsAll(req, res);
-  } else if (req.method === "POST" && req.url === "/admins/add") {
-    const { adminsAdd } = require("./admins-controller");
-    adminsAdd(req, res);
-  } else if (req.method === "GET" && req.url === "/films") {
-    const { FilmsAll } = require("./films-controller");
-    FilmsAll(req, res);
-  } else if (req.method === "POST" && req.url === "/films/add") {
-    const { FilmsAdd } = require("./films-controller");
-    FilmsAdd(req, res);
-  } else if (req.method === "GET" && req.url === "/categories") {
-    const { categoriesAll } = require("./categories-controller");
-    categoriesAll(req, res);
-  } else if (req.method === "POST" && req.url === "/categories/add") {
-    const { createCategories } = require("./categories-controller");
-    createCategories(req, res);
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: "Страницы не существует" }));
+  // Получаем путь из адреса
+  const path = req.url.match("^[^?]*")[0];
+  switch (true) {
+    case req.method === "GET" && path === "/admins":
+      const { adminsAll } = require("./admins-controller");
+      adminsAll(req, res, query);
+      break;
+    case req.method === "POST" && path === "/admins":
+      const { adminsAdd } = require("./admins-controller");
+      adminsAdd(req, res, body);
+      break;
+    case req.method === "GET" && path === "/films":
+      const { FilmsAll } = require("./films-controller");
+      FilmsAll(req, res, query);
+      break;
+    case req.method === "POST" && path === "/films":
+      const { FilmsAdd } = require("./films-controller");
+      FilmsAdd(req, res, body);
+      break;
+    case req.method === "GET" && path === "/categories":
+      const { categoriesAll } = require("./categories-controller");
+      categoriesAll(req, res, query);
+      break;
+    case req.method === "POST" && path === "/categories":
+      const { createCategories } = require("./categories-controller");
+      createCategories(req, res, body);
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Страницы не существует" }));
+      break;
   }
+
+  //   if (req.method === "GET" && path === "/admins") {
+  //     const { adminsAll } = require("./admins-controller");
+  //     adminsAll(req, res);
+  //   } else if (req.method === "POST" && path === "/admins/add") {
+  //     const { adminsAdd } = require("./admins-controller");
+  //     adminsAdd(req, res);
+  //   } else if (req.method === "GET" && path === "/films") {
+  //     const { FilmsAll } = require("./films-controller");
+  //     FilmsAll(req, res);
+  //   } else if (req.method === "POST" && path === "/films/add") {
+  //     const { FilmsAdd } = require("./films-controller");
+  //     FilmsAdd(req, res);
+  //   } else if (req.method === "GET" && path === "/categories") {
+  //     const { categoriesAll } = require("./categories-controller");
+  //     categoriesAll(req, res);
+  //   } else if (req.method === "POST" && path === "/categories/add") {
+  //     const { createCategories } = require("./categories-controller");
+  //     createCategories(req, res);
+  //   } else {
+  //     res.writeHead(404, { "Content-Type": "application/json" });
+  //     res.end(JSON.stringify({ error: "Страницы не существует" }));
+  //   }
 };
