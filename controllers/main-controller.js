ebccb6ebae4acb6c8bff3ecd4101ed59cf728e08
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-26 13:48:18 +0500

    fix cors error, set headers value

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -1,39 +1,40 @@
-const url = require('url')
-const Films = require('../models/films')
-const Categories = require('../models/categories');
-const {createAdmins} = require('./admins-controller')
+const url = require("url");
+const Films = require("../models/films");
+const Categories = require("../models/categories");
+const { createAdmins } = require("./admins-controller");
+
 module.exports = function requestController(req, res) {
-    if (req.method === 'GET' && req.url === '/admins') {
-        const {adminsAll} = require('./admins-controller')
-        adminsAll(req,res)
-    } 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    else if( req.method === 'POST' && req.url === '/admins/add'){
-        const {adminsAdd} = require('./admins-controller')
-        adminsAdd(req, res)
-    }
-    
-   
-    else if (req.method === 'GET' && req.url === '/films') {
-        const {FilmsAll} = require('./films-controller')
-        FilmsAll(req,res)
-    } 
-
-    else if (req.method === 'POST' && req.url === '/films/add') {
-        const {FilmsAdd} = require('./films-controller')
-        FilmsAdd(req,res)
-    } 
-    else if (req.method === 'GET' && req.url === '/categories') {
-        const {categoriesAll} = require('./categories-controller')
-        categoriesAll(req,res)
-    } 
-    else if(req.method === 'POST' && req.url === '/categories/add'){
-        const {createCategories} = require('./categories-controller')
-        createCategories(req,res)
-    }
-
-    else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Страницы не существует' }));
-    }
-}
+  if (req.method === "GET" && req.url === "/admins") {
+    const { adminsAll } = require("./admins-controller");
+    adminsAll(req, res);
+  } else if (req.method === "POST" && req.url === "/admins/add") {
+    const { adminsAdd } = require("./admins-controller");
+    adminsAdd(req, res);
+  } else if (req.method === "GET" && req.url === "/films") {
+    const { FilmsAll } = require("./films-controller");
+    FilmsAll(req, res);
+  } else if (req.method === "POST" && req.url === "/films/add") {
+    const { FilmsAdd } = require("./films-controller");
+    FilmsAdd(req, res);
+  } else if (req.method === "GET" && req.url === "/categories") {
+    const { categoriesAll } = require("./categories-controller");
+    categoriesAll(req, res);
+  } else if (req.method === "POST" && req.url === "/categories/add") {
+    const { createCategories } = require("./categories-controller");
+    createCategories(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Страницы не существует" }));
+  }
+};
