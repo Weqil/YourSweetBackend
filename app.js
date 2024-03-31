1c80bd72319ccf5a5f7f263090445f6e40b54055
MoskowPsix <bolshe.kivi@gmail.com>
2024-03-31 14:52:51 +0500

    add onMessage websocket

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const db = require('./base');
 const { WebSocketServer } = require('ws');
-const {connect, initWS} = require('./services/ws-service');
+const {connect, initWS, onMessage} = require('./services/ws-service');
 const http = require('http');
 const port = process.env.APP_PORT || 3500;
 const Sequelize = require('sequelize');
@@ -8,14 +8,15 @@ const requestController = require('./controllers/main-controller');
 
 
 const server = http.createServer(function(request, response, clients){
-    console.clear();
-    console.log('=================================');
-    console.log(`||     SERVER WORKED {$port}   ||`);
-    console.log('=================================');
+    // console.clear();
+    // console.log('=================================');
+    // console.log(`||     SERVER WORKED {$port}   ||`);
+    // console.log('=================================');
     requestController(request,response, clients);
 });
 
 initWS(server);
-connect()
+connect();
+onMessage();
 
 server.listen(port);
--- services/ws-service.js
+++ services/ws-service.js
@@ -11,16 +11,25 @@ module.exports.initWS = function (server) {
 }
 
 module.exports.connect = function () {
-    this.wsServer.on('connection', function(connection) {
-        const userId = uuidv4();
-        const ClientsWS = require('../models/ws_clients')
-        this.clients[userId] = connection;
-        connection.send(JSON.stringify({"message":"Подключение установлено."}))
-        ClientsWS.create({
-          ws_id: userId,
-          user_id: 1
-        });
+  this.wsServer.on('connection', function(connection) {
+      const userId = uuidv4();
+      const ClientsWS = require('../models/ws_clients')
+      this.clients[userId] = connection;
+      connection.send(JSON.stringify({"message":"Подключение установлено."}))
+      ClientsWS.create({
+        ws_id: userId,
+        user_id: 1
       });
+      connection.on('message', (message, isBinary) => {
+        console.log(message);
+      });
+    });
+}
+
+module.exports.onMessage = function () {
+  this.wsServer.on('message', (message) => {
+    console.log('получено сообщение: %s', message);
+  });
 }
 
 module.exports.getClients = function () {
